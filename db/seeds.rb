0c605a3046c57b89546150d7e6decb2578c6f179
Mutagah <kihahumutagah@gmail.com>
2024-01-27 18:53:51 +0300

    :card_file_box:feat:Addition of dummy data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Seeding Database tables....."
+    
+    ScannedData.create!({id: 1, scanData:"Mutagaismyname"})
+
+puts "!!End of seeding!!"
